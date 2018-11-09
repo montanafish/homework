968b69b2dad61327b541ce52815a9672c21f3c0f
Yasufabric <yasufabric1192@gmail.com>
2018-11-10 02:37:22 +0900

    added Card component

--- src/components/CordinatePage.js
+++ src/components/CordinatePage.js
@@ -1,32 +1,92 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
+import { Card, Row, Col } from "antd";
+
+const { Meta } = Card;
 
 export default class CordinatePage extends Component {
   render() {
     return (
-      <div>
-        <Query query={USERS_INFO}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-
-            return data.users.map(user => (
-              <div key={user.user_id}>
+      <Fragment>
+        <Row>
+          <Col span={8}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
                 <img
-                  src={user.image_url}
-                  alt="{use.name}"
-                  style={{ maxHeight: "200px", maxWidth: "200px" }}
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
                 />
-                <p>{`${user.user_id}: ${user.name}`}</p>
-                <p>{`${user.height} : ${user.gender} : ${user.birth_place}`}</p>
-              </div>
-            ));
-          }}
-        </Query>
-        <h1>コーディネーター</h1>
-        コーディネーターのページです きてます
-      </div>
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+        </Row>
+        <div>
+          <Query query={USERS_INFO}>
+            {({ loading, error, data }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error :(</p>;
+
+              return data.users.map(user => (
+                <div key={user.user_id}>
+                  <img
+                    src={user.image_url}
+                    alt="{use.name}"
+                    style={{ maxHeight: "200px", maxWidth: "200px" }}
+                  />
+                  <p>{`${user.user_id}: ${user.name}`}</p>
+                  <p>{`${user.height} : ${user.gender} : ${
+                    user.birth_place
+                  }`}</p>
+                </div>
+              ));
+            }}
+          </Query>
+          <h1>コーディネーター</h1>
+          コーディネーターのページです きてます
+        </div>
+      </Fragment>
     );
   }
 }
