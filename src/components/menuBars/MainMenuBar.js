7e8bf9124a6cdccfacf099f4e4b56fa45c096e40
montanafish <mitch@officesato.com>
2018-11-15 17:49:22 +1100

    fixed top menu style

--- src/components/menuBars/MainMenuBar.js
+++ src/components/menuBars/MainMenuBar.js
@@ -1,79 +1,31 @@
-import React, { Component } from "react";
-import { Menu } from "antd";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { Menu } from 'antd'
+import { Link } from 'react-router-dom'
 
 export default class MainMenuBar extends Component {
   render() {
     return (
-      <Menu
-        mode="horizontal"
-        defaultSelectedKeys={["1"]}
-        style={{
-          mode: "inline",
-          textAlign: "center"
-        }}
-      >
-        <Menu.Item
-          key="1"
-          style={{
-            width: "160px",
-            height: "46px",
-            fontSize: "17px",
-            fontFamily:
-              ' "Helvetica Neue", "Helvetica", "Arial", "Verdana", sans-serif'
-          }}
-        >
-          <Link to="/Cordinate">ALL</Link>
-        </Menu.Item>
-        <Menu.Item
-          key="2"
-          style={{
-            width: "160px",
-            height: "46px",
-            fontSize: "17px",
-            fontFamily:
-              ' "Helvetica Neue", "Helvetica", "Arial", "Verdana", sans-serif'
-          }}
-        >
-          <Link to="/">MEN</Link>
-        </Menu.Item>
-        <Menu.Item
-          key="3"
-          style={{
-            width: "160px",
-            height: "46px",
-            fontSize: "17px",
-            fontFamily:
-              ' "Helvetica Neue", "Helvetica", "Arial", "Verdana", sans-serif'
-          }}
-        >
-          <Link to="/UserCordinater">WOMEN</Link>
-        </Menu.Item>
-        <Menu.Item
-          key="4"
-          style={{
-            width: "160px",
-            height: "46px",
-            fontSize: "17px",
-            fontFamily:
-              ' "Helvetica Neue", "Helvetica", "Arial", "Verdana", sans-serif'
-          }}
-        >
-          <Link to="/kidsCordinate">KIDS</Link>
-        </Menu.Item>
-        <Menu.Item
-          key="5"
-          style={{
-            width: "160px",
-            height: "46px",
-            fontSize: "17px",
-            fontFamily:
-              ' "Helvetica Neue", "Helvetica", "Arial", "Verdana", sans-serif'
-          }}
-        >
-          <Link to="worldCordinate">WORLD</Link>
-        </Menu.Item>
-      </Menu>
-    );
+      <div id="switch_sex_type">
+        <nav>
+          <ul>
+            <li class="all current">
+              <a href="?c=SwitchTabType">ALL</a>
+            </li>
+            <li class="men ">
+              <a href="?c=SwitchTabType&amp;sex_id=1">MEN</a>
+            </li>
+            <li class="women ">
+              <a href="?c=SwitchTabType&amp;sex_id=2">WOMEN</a>
+            </li>
+            <li class="kids ">
+              <a href="?c=SwitchTabType&amp;sex_id=3">KIDS</a>
+            </li>
+            <li class="world ">
+              <a href="?c=SwitchTabType&amp;sex_id=4">WORLD</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
   }
 }
