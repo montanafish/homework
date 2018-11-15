6e59dfc847674790d933ff128e0147d9261f3e20
montanafish <mitch@officesato.com>
2018-11-15 17:54:54 +1100

    changed url routes

--- src/components/menuBars/MainMenuBar.js
+++ src/components/menuBars/MainMenuBar.js
@@ -9,19 +9,19 @@ export default class MainMenuBar extends Component {
         <nav>
           <ul>
             <li class="all current">
-              <a href="?c=SwitchTabType">ALL</a>
+              <a href="/">ALL</a>
             </li>
             <li class="men ">
-              <a href="?c=SwitchTabType&amp;sex_id=1">MEN</a>
+              <a href="/men">MEN</a>
             </li>
             <li class="women ">
-              <a href="?c=SwitchTabType&amp;sex_id=2">WOMEN</a>
+              <a href="/women">WOMEN</a>
             </li>
             <li class="kids ">
-              <a href="?c=SwitchTabType&amp;sex_id=3">KIDS</a>
+              <a href="/kids">KIDS</a>
             </li>
             <li class="world ">
-              <a href="?c=SwitchTabType&amp;sex_id=4">WORLD</a>
+              <a href="/world">WORLD</a>
             </li>
           </ul>
         </nav>
--- src/containers/Wear/index.js
+++ src/containers/Wear/index.js
@@ -1,31 +1,31 @@
-import React, { Component, Fragment } from "react";
-import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
-import "./index.css";
+import React, { Component, Fragment } from 'react'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+import './index.css'
 
-import MainMenuBar from "../../components/menuBars/MainMenuBar";
-import MainSideBar from "../../components/sideBars/MainSideBar";
-import TopBanner from "../../components/header/TopBanner";
-import HeaderContent from "../../components/header/HeaderContent";
-import SwitchMenu from "../../components/SwitchMenu";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import TopPage from "../../components/pages/MainPage";
-import UserCordinaterPage from "../../components/pages/UserCordinaterPage";
-import CordinatePage from "../../components/pages/Page1";
-import NoPage from "../../components/pages/NoPage";
+import MainMenuBar from '../../components/menuBars/MainMenuBar'
+import MainSideBar from '../../components/sideBars/MainSideBar'
+import TopBanner from '../../components/header/TopBanner'
+import HeaderContent from '../../components/header/HeaderContent'
+import SwitchMenu from '../../components/SwitchMenu'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import TopPage from '../../components/pages/MainPage'
+import UserCordinaterPage from '../../components/pages/UserCordinaterPage'
+import CordinatePage from '../../components/pages/Page1'
+import NoPage from '../../components/pages/NoPage'
 
-import { Row, Layout } from "antd";
+import { Row, Layout } from 'antd'
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Sider, Content } = Layout
 
 const client = new ApolloClient({
-  uri: "https://hamlet-staging.herokuapp.com/v1alpha1/graphql"
-});
+  uri: 'https://hamlet-staging.herokuapp.com/v1alpha1/graphql',
+})
 
 class Wear extends Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {}
   }
 
   render() {
@@ -36,11 +36,11 @@ class Wear extends Component {
             <Layout>
               <Header
                 style={{
-                  position: "fixed",
+                  position: 'fixed',
                   zIndex: 100,
-                  width: "100%",
-                  boxShadow: "0 0 6px rgba(0, 0, 0, 0.3)",
-                  opacity: 0.9
+                  width: '100%',
+                  boxShadow: '0 0 6px rgba(0, 0, 0, 0.3)',
+                  opacity: 0.9,
                 }}
               >
                 <HeaderContent />
@@ -51,34 +51,26 @@ class Wear extends Component {
               <Layout>
                 <MainMenuBar />
               </Layout>
-              <Layout style={{ padding: "10px 0 0 0" }} />
+              <Layout style={{ padding: '10px 0 0 0' }} />
               <SwitchMenu />
               <Layout>
                 <div
                   style={{
                     width: 990,
                     height: 3000,
-                    margin: "30px auto 0 auto"
+                    margin: '30px auto 0 auto',
                   }}
                 >
-                  <Sider width={180} style={{ float: "left" }}>
+                  <Sider width={180} style={{ float: 'left' }}>
                     <MainSideBar />
                   </Sider>
-                  <Content
-                    style={{ width: 780, float: "right", display: "block" }}
-                  >
+                  <Content style={{ width: 780, float: 'right', display: 'block' }}>
                     <Switch>
                       <Route path="/" exact component={TopPage} />
-                      <Route
-                        path="/Cordinate"
-                        exact
-                        component={CordinatePage}
-                      />
-                      <Route
-                        path="/UserCordinater"
-                        exact
-                        component={UserCordinaterPage}
-                      />
+                      <Route path="/men" exact component={CordinatePage} />
+                      <Route path="/women" exact component={UserCordinaterPage} />
+                      <Route path="/kids" exact component={CordinatePage} />
+                      <Route path="/world" exact component={CordinatePage} />
                       <Route exact component={NoPage} />
                     </Switch>
                   </Content>
@@ -87,9 +79,9 @@ class Wear extends Component {
               <Row
                 style={{
                   height: 252,
-                  borderTop: "1px #ddd solid",
-                  background: "#f6f7f8",
-                  padding: "30px 0 0 0"
+                  borderTop: '1px #ddd solid',
+                  background: '#f6f7f8',
+                  padding: '30px 0 0 0',
                 }}
               >
                 <div />
@@ -98,18 +90,18 @@ class Wear extends Component {
               <Row
                 style={{
                   height: 235,
-                  borderTop: "1px #ddd solid",
-                  background: "#e8e8e8"
+                  borderTop: '1px #ddd solid',
+                  background: '#e8e8e8',
                 }}
               >
                 <div />
               </Row>
               <Footer
                 style={{
-                  textAlign: "center",
-                  borderTop: "1px #ddd solid",
+                  textAlign: 'center',
+                  borderTop: '1px #ddd solid',
                   height: 356,
-                  padding: "52px 0 52px 0"
+                  padding: '52px 0 52px 0',
                 }}
               >
                 COPYRIGHT ©XXXX Technologies
@@ -118,8 +110,8 @@ class Wear extends Component {
           </Fragment>
         </Router>
       </ApolloProvider>
-    );
+    )
   }
 }
 
-export default Wear;
+export default Wear
